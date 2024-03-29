a9d9822a1ccddf7eaa0a2e3a21d038be2d4795f1
Aakarshit07 <aakarshit070@gmail.com>
2024-03-30 00:39:53 +0530

    one word not fond case fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,16 @@ function App() {
 
 
   const handleSearch = () => {
-    if (word) { 
-      let result = dictionary.filter((inputword) => inputword.word.toLowerCase().includes(word.toLowerCase()))
-      if(result.length > 0) {
-        setWordMeaning(result[0].meaning);
-      } else {
-       setWordMeaning("Word not found in the dictionary.");
-      }
-    } else {
-      setWordMeaning("");
-    }    
+  if (word) { 
+    let result = dictionary.filter((inputword) => inputword.word.toLowerCase().includes(word.toLowerCase()))
+    if(result.length > 0) {
+      setWordMeaning(result[0].meaning);
+    }
   }
+  else {
+    setWordMeaning("Word not found in the dictionary.");
+  }  
+}   
 
   return (
     <>  
